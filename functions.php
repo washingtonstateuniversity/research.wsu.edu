82d817535aac4c4a164bb70b8fa8ef6b468fd45a
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-09-17 13:58:15 -0700

    Rename to not confuse with custom editor
    Rename to not confuse with custom editor

--- functions.php
+++ functions.php
@@ -5,5 +5,5 @@ add_action( 'wp_enqueue_scripts', 'research_enqueue_scripts');
  * Enqueue custom scripting in child theme.
  */
 function research_enqueue_scripts() {
-	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/assets/custom.css' );
+	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/assets/custom-research.css' );
 }
