caafc8c34ae04cb6fae233d33dd94e6befd62234
Phil Cable <pcable@wsu.edu>
2017-02-20 08:46:17 -0800

    Update JavaScript so SVGs in the viewport animate on page load

--- js/animate-svg.js
+++ js/animate-svg.js
@@ -10,34 +10,25 @@
 	}
 
 	/**
-	 * When an element to be animated is scrolled into the viewport, remove the `animate`
+	 * When an element to be animated is in the viewport, remove the `animate`
 	 * class from it and add the `animated` class to its list items at short intervals.
 	 */
-	function animateElement() {
-		var $elementsToAnimate = $( ".animate" );
+	var $elementsToAnimate = $( ".animate" );
 
-		$elementsToAnimate.each( function() {
-			var $element = $( this ),
-				bars = this.getElementsByTagName( "animate" );
+	$elementsToAnimate.each( function() {
+		var $element = $( this ),
+			bars = this.getElementsByTagName( "animate" );
 
-			$( document ).on( "scroll", function() {
-				if ( !inViewport( $element ) || $element.hasClass( "animated" ) ) {
-					return;
-				}
+		$( document ).on( "scroll", function() {
+			if ( !inViewport( $element ) || $element.hasClass( "animated" ) ) {
+				return;
+			}
 
-				for ( var i = 0; i < bars.length; i++ ) {
-					bars[ i ].beginElement();
-				}
+			for ( var i = 0; i < bars.length; i++ ) {
+				bars[ i ].beginElement();
+			}
 
-				$element.addClass( "animated" );
-			} );
+			$element.addClass( "animated" );
 		} );
-	}
-
-	/**
-	 * Fire any actions that we need to happen once the document is ready.
-	 */
-	$( document ).ready( function() {
-		animateElement();
 	} );
 }( jQuery, window, document ) );
