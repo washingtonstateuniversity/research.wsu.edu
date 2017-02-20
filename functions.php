f2234847d7de6e8f52afec1adf5f6aed52144a58
Phil Cable <pcable@wsu.edu>
2017-02-20 10:50:40 -0800

    Bump version in `functions.php`

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ class WSU_Research_Theme {
 	 *
 	 * @var string String used for busting cache on scripts.
 	 */
-	var $script_version = '0.3.0';
+	var $script_version = '0.3.2';
 
 	/**
 	 * @since 0.3.0
